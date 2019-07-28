36b768b6b0ed90385073cf5618ab103d50e47c01
sidirovich <sidirovich-van@yandex.ru>
2019-07-28 10:58:46 +0300

    first commit

---
+++ build/precache-manifest.265f5d76b3dea88c9bfafe6988de3873.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ad551e087523084f21cb66c7f344823",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8a2bec12128347ab297",
+    "url": "/static/css/main.3c104e85.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d8a2bec12128347ab297",
+    "url": "/static/js/main.28647029.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
